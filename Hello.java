9f3e653ddec7337a6754ccfdf9fd2849e38f0419
ryanseo92 <84111413+ryanseo92@users.noreply.github.com>
2021-05-17 13:02:02 +0900

    Delete Hello.java

--- Hello.java
+++
@@ -1,9 +0,0 @@
-public class Hello 
-{
-  public static void main(String[] args)
-         {
-            System.out.println("Hello, welcome to the java world!");
-
-            System.out.println("i'm engineer")
-         }
-}
